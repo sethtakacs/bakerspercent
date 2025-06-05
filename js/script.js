699853875391e20038da9dd2b94b216e851f060a
sethtakacs <63225004+sethtakacs@users.noreply.github.com>
2025-06-05 11:48:06 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,1 @@
+function navigateToRecipe() { const dropdown = document.getElementById('recipeSelect'); const selectedValue = dropdown.value; if (selectedValue) { window.location.href = selectedValue; } }
